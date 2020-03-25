db0a9e5588bd1f96d524154413f1503cdcdb4f54
Carlos <carlos.erena.ibanez@iesjandula.es>
2020-03-25 12:16:29 +0100

    Revert "Cambio de la propiedad ID en el POJO Cuentas.java"
    This reverts commit a02353ff

--- src/main/java/com/erena/restapi/entities/Cuentas.java
+++ src/main/java/com/erena/restapi/entities/Cuentas.java
@@ -4,14 +4,13 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
 @Entity
 public class Cuentas {
 
-    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long id;
+    private @Id @GeneratedValue long id;
     private String usuario;
     private String contraseña;
 
